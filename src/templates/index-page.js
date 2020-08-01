818f97f66425a3526882ac9a54847061ab4f5f74
Monkeystation <job@monkeystation.nl>
2020-08-01 12:30:13 +0800

    First changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
   image,
@@ -15,6 +13,129 @@ export const IndexPageTemplate = ({
   description,
   intro,
 }) => (
+  <div>
+    <section className="section intro py-0" >
+      <div className="has-background-light">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-5-desktop is-4 is-offset-4 py-6">
+              <h5 class="subtitle green-text has-text-weight-bold is-uppercase">powered by the sun</h5>
+              <h1 className="title blue-text has-text-weight-bold is-size-5-mobile is-size-4-tablet is-size-3-desktop is-size-3-fullhd">
+                We transform CO₂ into renewable carbon compounds.
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section mission">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-12 has-background-light">
+            <div className="column is-12 py-6">
+            <h5 class="subtitle blue-text has-text-weight-bold is-uppercase">what we stand for</h5>
+            <h1 className="title is-family-secondary green-text has-text-weight-bold is-size-4-mobile is-size-3-tablet is-size-2-desktop is-size-2-fullhd">
+              Caring for a bright future on this planet, we believe in restoring balance through human ingenuity.
+            </h1>
+            <button class="button">
+              <span class="icon">
+                <i class="fab fa-github"></i>
+              </span>
+              <span>Watch brandmovie</span>
+            </button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section animation">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-12 has-background-light">
+            <h5 class="subtitle blue-text has-text-weight-bold is-uppercase">what we are working on</h5>
+            <h1 className="title is-family-secondary green-text has-text-weight-bold is-size-3">
+              The future is circulair
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section timeline has-background-primary" style={{height:'500px'}}>
+      <div className="columns">
+        <div className="column is-12">
+          <h5 class="subtitle white-text has-text-weight-bold is-uppercase">it is looking bright</h5>
+          <h1 className="title is-family-secondary green-text has-text-weight-bold is-size-3">
+            Past, present and future
+          </h1>
+        </div>
+      </div>
+    </section>
+    <section className="section how">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-12 has-background-light">
+            <h5 class="subtitle blue-text has-text-weight-bold is-uppercase">this is how we do it</h5>
+            <h1 className="title is-family-secondary green-text has-text-weight-bold is-size-3">
+              Create natural all-in-one mini factories
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section infographic">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-12 has-background-dark" style={{height:'500px'}}>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section team has-background-light" style={{height:'500px'}}>
+    </section>
+    <section className="section partners">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-12 has-background-dark" style={{height:'500px'}}>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  
+  
+)
+
+IndexPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+}
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <IndexPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+      />
+    </Layout>
+  )
+}
+
+/*
   <div>
     <div
       className="full-width-image margin-top-0"
@@ -68,7 +189,7 @@ export const IndexPageTemplate = ({
       <div className="container">
         <div className="section">
           <div className="columns">
-            <div className="column is-10 is-offset-1">
+            <div className="column is-10 is-offset-1">  
               <div className="content">
                 <div className="content">
                   <div className="tile">
@@ -112,37 +233,7 @@ export const IndexPageTemplate = ({
       </div>
     </section>
   </div>
-)
-
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
-
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
-
-  return (
-    <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
-    </Layout>
-  )
-}
+  */
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
