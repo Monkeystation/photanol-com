46c9f64971fe3bb82c0741cc979d0c9a559c3018
Monkeystation <job@monkeystation.nl>
2020-08-21 09:01:23 +0800

    roadmap mobile

--- src/components/RoadmapItem.js
+++ src/components/RoadmapItem.js
@@ -11,6 +11,78 @@ const RoadmapItem = (props) => {
   if (isMobile) {
     return (
       <div key={index} className="item" style={{width: width}} id={"item" + index} onClick={onClick}>
+        <div className="item-elements">
+          <div className="item-element-top">
+            <h4 className="white-text has-text-centered has-text-weight-bold item-title">
+              {title}
+            </h4>
+          </div>             
+          <div className="item-element-center">
+            <img 
+              className="item-icon" 
+              src={PreviewCompatibleFile(icon)} 
+            />
+            <img className="item-image" 
+              style={{ opacity: opacity }} 
+              src={PreviewCompatibleFile(image)}
+            />
+          </div>
+          <div className="item-element-bottom">
+            <h2 className="blue-300-text has-text-weight-bold has-text-centered item-year">
+              {year}
+            </h2>
+            <div 
+              className="item-text white-text" 
+              style={{ display: 'block', opacity: opacity }} 
+              dangerouslySetInnerHTML={{__html: converter.makeHtml(text)}}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div key={index} className="item" style={{width: width}} id={"item" + index} onClick={onClick}>
+        <div className="item-elements">
+          <div className="item-element-top">
+            <h4 className="white-text has-text-centered has-text-weight-bold item-title">
+              {title}
+            </h4>
+          </div>             
+          <div className="item-element-center">
+            <img 
+              className="item-icon" 
+              style={{ width: imageSize, height: imageSize }} 
+              src={PreviewCompatibleFile(icon)} 
+            />
+            <img className="item-image" 
+              style={{ width: imageSize, height: imageSize, opacity: opacity }} 
+              src={PreviewCompatibleFile(image)}
+            />
+          </div>
+          <div className="item-element-bottom">
+            <h2 className="blue-300-text has-text-weight-bold has-text-centered item-year">
+              {year}
+            </h2>
+          </div>
+        </div>
+        <div 
+          className="item-text white-text" 
+          style={{ display: showText ? 'block' : 'none', opacity: opacity }} 
+          dangerouslySetInnerHTML={{__html: converter.makeHtml(text)}}
+        />
+      </div>
+    )
+  }
+}
+
+/*
+const RoadmapItem = (props) => {
+  const {isMobile, index, width, onClick, imageSize, showText, opacity, title, icon, image, year, text} = props
+  
+  if (isMobile) {
+    return (
+      <div key={index} className="item" style={{width: width}} id={"item" + index} onClick={onClick}>
         <div className="item-elements" style={{opacity: opacity}}>
           <div className="item-element-top">
             <h4 className="white-text has-text-centered has-text-weight-bold item-title">
@@ -67,5 +139,5 @@ const RoadmapItem = (props) => {
     )
   }
 }
-
+*/
 export default RoadmapItem
