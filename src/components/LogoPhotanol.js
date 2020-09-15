58420fae72bff512f239c79646b5b345c9003f8e
Monkeystation <job@monkeystation.nl>
2020-09-15 15:23:25 +0800

    svg animation logo

--- src/components/LogoPhotanol.js
+++ src/components/LogoPhotanol.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { withPrefix } from 'gatsby'
 
 const LogoPhotanol = () => {
-
+  
+  return (
+    <object id="logo-photanol" data={`${withPrefix('/')}img/logo-ani-web.svg`} type="image/svg+xml" />
+  )
+  
+  /*
   return (
     <video id="logo-photanol" playsInline autoPlay muted>
       <source src={`${withPrefix('/')}img/logo-animation.mp4`} type="video/mp4" />
     </video>
   )
+  */
 }
 
 export default LogoPhotanol
